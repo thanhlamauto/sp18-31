5fe37ba844d12435c056364b3e7789f7b6eb9ade
Thanh Lâm Nguyễn <thanhlamtba@gmail.com>
2024-02-07 14:14:46 +0700

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,11 @@
+public class HelloNumber {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            sum += x;
+            System.out.print(sum + " ");
+            x++;
+        }
+    }
+}
